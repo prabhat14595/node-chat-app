0d30f0fbd99221c8ff77036b8901c6f8a5ba2585
Prabhat <prabhat.ranjan,pr@gmail.com>
2018-10-20 11:43:58 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path= require('path');
+const express= require('express');
+
+const publicPath= path.join(__dirname, '../public');
+//console.log(publicPath);
+var app = express();
+app.use(express.static(publicPath));
+
+app.listen(3000, ()=>{
+    console.log("server is up at port 3000");
+});
+
+
